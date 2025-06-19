2d3b5a71150bf00827e3e6aa81825d0aced19413
Lunick Francois <LunickFrancois22@gmail.com>
2025-06-19 19:20:56 -0400

    Add HTTP request logic to fetch_parse function
    Implemented GET request using the requests library.

--- player_stats_scraper.py
+++ player_stats_scraper.py
@@ -1,3 +1,5 @@
+import requests
+
 url = "https://www.basketball-reference.com/leagues/NBA_2025_per_game.html"
 
 def fetch_parse(url):
@@ -15,3 +17,16 @@ def fetch_parse(url):
     Raises:
         Exception: If the request fails
     """
+    
+    try:
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            print("GET request successful")
+        else:
+            print(f"GET request failed with status code: {response.status_code}")
+            print(f"Response text: {response.text}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        
+fetch_parse(url)
