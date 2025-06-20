cf331eaffec26ce04e65f4b487f4db7587880f0d
Lunick Francois <LunickFrancois22@gmail.com>
2025-06-19 20:01:56 -0400

    Extract per_game_stats table from HTML comments

--- player_stats_scraper.py
+++ player_stats_scraper.py
@@ -1,9 +1,10 @@
 import requests
+from bs4 import BeautifulSoup, Comment
 
 url = "https://www.basketball-reference.com/leagues/NBA_2025_per_game.html"
 
 def fetch_parse(url):
-    """_summary_
+    """Fetches NBA player stats from the Basketball reference url
 
     Args:
         url (string): the url to scrape NBA stats from
@@ -19,11 +20,29 @@ def fetch_parse(url):
     """
     
     try:
+        # Send an HTTP GET request to the URL
         response = requests.get(url)
         
         if response.status_code == 200:
             print("GET request successful")
+            
+            soup = BeautifulSoup(response.content, 'html5lib')
+            print(soup.prettify())
+            
+            # Find all of the HTML comments in the page
+            comments = soup.find_all(string=lambda text: isinstance(text, Comment))
+            print(comments)
+            
+            for comment in comments:
+                if 'per_game_stats' in comment:
+                    # Parse the comment's content as HTML
+                    new_soup = BeautifulSoup(comment, 'html5lib')
+                    # Find the stats table
+                    table = new_soup.find('table', id='per_game_stats')
+                    
+                    print(table)
         else:
+            # Print status code and response if the request is not successful
             print(f"GET request failed with status code: {response.status_code}")
             print(f"Response text: {response.text}")
     except requests.exceptions.RequestException as e:
