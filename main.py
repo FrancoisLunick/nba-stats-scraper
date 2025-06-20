2027fcfd4dec36ae1b0f0e8084ce2d244fcd8f97
Lunick Francois <LunickFrancois22@gmail.com>
2025-06-20 14:58:07 -0400

    Remove url

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ from player_stats_scraper import fetch_parse
 from visualize_stats import generate_plots
 
 file_name= "data/2025_player_stats.csv"
-url = "https://www.basketball-reference.com/leagues/NBA_2025_per_game.html"
 
 def main():
     # Scrape NBA data
-    fetch_parse(url)
+    fetch_parse()
     
     # Generate Plots
     generate_plots(file_name)
