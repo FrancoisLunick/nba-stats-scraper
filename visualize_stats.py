ea71e62d1b194a7433b1c4a3f15146113f656f0a
Lunick Francois <LunickFrancois22@gmail.com>
2025-06-19 23:23:55 -0400

    Add bar chart visualization for top 10 players by points

--- visualize_stats.py
+++ visualize_stats.py
@@ -1,4 +1,5 @@
 import pandas
+import matplotlib.pyplot
 
 # Read the CSV file into DataFrame
 data_frame = pandas.read_csv("data/2025_player_stats.csv")
@@ -12,5 +13,10 @@ for col in numeric_cols:
 
 data_sorted = data_frame.sort_values(by='PTS', ascending=False)    
 top_10_players = data_sorted.head(10)
+
+top_10_players.plot.barh(x='Player', y='PTS', title="Top 10 Players")
+matplotlib.pyplot.xlabel("PTS")
+matplotlib.pyplot.ylabel("Player")
+matplotlib.pyplot.show()
 
 print(top_10_players)
